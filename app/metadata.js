6949ead80cefb24b15e1fc41d4e6941fafc2c61c
Shashank Phatkure <shashankphatkure@gmail.com>
2024-12-01 04:02:24 +0530

    updated

---
+++ app/metadata.js
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: "Driver App",
+  description: "Delivery Driver Application",
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: "Driver App",
+  },
+  icons: {
+    icon: "/icons/icon-192x192.png",
+    shortcut: "/icons/icon-192x192.png",
+    apple: "/icons/icon-192x192.png",
+  },
+};
